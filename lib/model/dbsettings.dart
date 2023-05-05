813e7637ea3b16e3828c3e24b02a974a3a8fc5a2
IRDnamik <namik.scherzl@ird.fr>
2023-05-05 16:30:41 +0400

    add dbSetting class

---
+++ lib/model/dbsettings.dart
@@ -0,0 +1,8 @@
+class DbSetting {
+  final String databaseName;
+  final String host;
+  final String user;
+  final String password;
+
+  DbSetting(this.databaseName, this.host, this.user, this.password);
+}
