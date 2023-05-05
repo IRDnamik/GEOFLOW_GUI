14ee6fa96a5795a8365640b3a322cb8920fe3702
IRDnamik <namik.scherzl@ird.fr>
2023-05-05 10:58:49 +0400

    restructuration of the views

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geoflow_gui/services/process.dart';
-import './ui/color.dart';
-import './ui/diagram';
+import 'package:geoflow_gui/ui/views/main_view.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,73 +12,77 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatelessWidget {
-  final diagram = Diagramme();
-  final process = Process();
-  var selectedDir;
+// class MainPage extends StatelessWidget {
+//   final diagram = Diagramme();
+//   final process = Process();
+//   var selectedDir;
 
-  MainPage({super.key});
+//   MainPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: null,
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                MyThemeColor.darkGrey,
-                MyThemeColor.darkViolet,
-                MyThemeColor.darkGrey
-              ]),
-        ),
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Spacer(),
-              diagram.buildBox('Input', () {
-                showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    title: const Text('Select Data sources'),
-                    actions: <Widget>[
-                      GestureDetector(
-                          onTap: () {},
-                          child: Image.asset('assets/pgsql.png',
-                              width: 128, height: 128)),
-                      TextButton(
-                        onPressed: () => Navigator.pop(context, 'Cancel'),
-                        child: const Text('Cancel'),
-                      ),
-                      // TextButton(
-                      //   onPressed: () => Navigator.pop(context, 'OK'),
-                      //   child: const Text('OK'),
-                      // ),
-                    ],
-                  ),
-                );
-                // selectedDir = process.selectDirectory();
-              }), // open file explorer
-              const Spacer(),
-              Align(
-                  alignment: const Alignment(0, 0.1),
-                  child: Container(
-                    child: diagram.buildBox('Geoflow', () {}),
-                  )), // open page to generate json file
-              const Spacer(),
-              diagram.buildBox(
-                  'Output', () {}), // open page to set url and password
-              const Spacer(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: null,
+//       body: Container(
+//         decoration: const BoxDecoration(
+//           gradient: LinearGradient(
+//               begin: Alignment.topLeft,
+//               end: Alignment.bottomRight,
+//               colors: [
+//                 MyThemeColor.darkGrey,
+//                 MyThemeColor.darkViolet,
+//                 MyThemeColor.darkGrey
+//               ]),
+//         ),
+//         child: Center(
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               const Spacer(),
+//               diagram.buildBox('Input', () {
+//                 showDialog<String>(
+//                   context: context,
+//                   builder: (BuildContext context) => AlertDialog(
+//                     title: const Text('Select Data sources'),
+//                     actions: <Widget>[
+//                       GestureDetector(
+//                           onTap: () {}, // redirect  to pick directory
+//                           child: Image.asset('assets/pgsql.png',
+//                               width: 128, height: 128)),
+//                       GestureDetector(
+//                           onTap: () {}, // redirect  to DB connection
+//                           child: Image.asset('assets/pgsql.png',
+//                               width: 128, height: 128)),
+//                       TextButton(
+//                         onPressed: () => Navigator.pop(context, 'Cancel'),
+//                         child: const Text('Cancel'),
+//                       ),
+//                       // TextButton(
+//                       //   onPressed: () => Navigator.pop(context, 'OK'),
+//                       //   child: const Text('OK'),
+//                       // ),
+//                     ],
+//                   ),
+//                 );
+//                 // selectedDir = process.selectDirectory();
+//               }), // open file explorer
+//               const Spacer(),
+//               Align(
+//                   alignment: const Alignment(0, 0.1),
+//                   child: Container(
+//                     child: diagram.buildBox('Geoflow', () {}),
+//                   )), // open page to generate json file
+//               const Spacer(),
+//               diagram.buildBox(
+//                   'Output', () {}), // open page to set url and password
+//               const Spacer(),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 // showDialog(
 //   context: context,
