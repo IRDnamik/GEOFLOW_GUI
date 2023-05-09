7cd5f862f1a438327e1118f9bf6dce377a1cce7e
IRDnamik <namik.scherzl@ird.fr>
2023-05-09 10:39:53 +0400

    add dbmanager

--- lib/model/dbsettings.dart
+++ lib/model/dbsettings.dart
@@ -1,8 +1,14 @@
 class DbSetting {
+  final int? id;
   final String databaseName;
   final String host;
   final String user;
   final String password;
 
-  DbSetting(this.databaseName, this.host, this.user, this.password);
+  DbSetting(
+      {this.id,
+      required this.databaseName,
+      required this.host,
+      required this.user,
+      required this.password});
 }
